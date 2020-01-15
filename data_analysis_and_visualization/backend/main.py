abbbc1b3dc4b3d7c548012e1d2ea5783f2160cca
Hextar <ste13456@gmail.com>
2020-01-15 18:38:40 +0100

    dockerized BE

--- data_analysis_and_visualization/backend/main.py
+++ data_analysis_and_visualization/backend/main.py
@@ -1,1 +1,16 @@
-from src import app
+# coding=utf-8
+from flask import Flask
+from flask_cors import CORS
+
+
+# creating the Flask application
+app = Flask(__name__,
+    static_folder='./backend/app/static',
+    template_folder='./backend/app/templates')
+
+# allowing cors for google-chrome
+CORS(app)
+
+# run app
+if __name__ == "__main__":
+	app.run(debug=True, host='0.0.0.0', port=5000)
